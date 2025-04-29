86d3bb5f9c526f905ee5accc71428f1c53939b8e
Shahinoor Shahin <shahin@braincraftapps.com>
2025-04-29 09:06:44 +0600

    Changed github deployment

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['medium.com', 'cdn-images-1.medium.com'],
   },
+  output: 'export',
+  basePath: '/sshahin0.github.io',
+  assetPrefix: '/sshahin0.github.io/',
 }
 
 module.exports = nextConfig 
